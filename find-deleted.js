a5315e0a73d9ab7573baddb0bd1853359f4da78c
statechny <statechnyi.s@gmail.com>
2023-02-10 15:34:43 +0200

    rename finction

--- find-deleted.js
+++ find-deleted.js
@@ -2,21 +2,17 @@ const array = [...Array(1000004).keys()];
 
 const arrayWithDeletedItem = array.filter((number) => number !== 77);
 
-let count = 0;
-
-const recursiveBinarySearch = (arr, start, end) => {
+const recursiveBinaryFindDeleted = (arr, start, end) => {
     let mid = Math.floor((start + end) / 2);
     const isShift = arr[mid] !== mid
-    count++;
     if ( end - start < 2) {
         return Math.min(arr.length, (isShift ? arr[mid] - 1 : arr[mid] + 1))
     }
     if(isShift) {
-        return recursiveBinarySearch(arr, start, mid - 1);
+        return recursiveBinaryFindDeleted(arr, start, mid - 1);
     }  else {
-        return recursiveBinarySearch(arr, mid + 1, end);
+        return recursiveBinaryFindDeleted(arr, mid + 1, end);
     }
 }
 
-console.log({recursiveBinarySearch: recursiveBinarySearch(arrayWithDeletedItem, 0, arrayWithDeletedItem.length)});
-console.log({count});
+console.log({recursiveBinarySearch: recursiveBinaryFindDeleted(arrayWithDeletedItem, 0, arrayWithDeletedItem.length)});
